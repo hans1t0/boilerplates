c11cc14a05a5770f2c4dc2e61039044bdee32441
hans1t0 <hcdesign14@gmail.com>
2022-12-01 20:31:15 +0100

    com2

---
+++ proxmox/ubuntu-template.sh
@@ -0,0 +1,15 @@
+# remove existing image in case last execution did not complete successfully
+rm jammy-server-cloudimg-amd64.img
+wget https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img
+sudo virt-customize -a jammy-server-cloudimg-amd64.img --install qemu-guest-agent
+sudo qm create 8000 --name "ubuntu-2204-cloudinit-template" --memory 2048 --cores 2 --net0 virtio,bridge=vmbr0
+sudo qm importdisk 8000 jammy-server-cloudimg-amd64.img local-lvm
+sudo qm set 8000 --scsihw virtio-scsi-pci --scsi0 local-lvm:vm-9000-disk-1
+sudo qm set 8000 --boot c --bootdisk scsi0
+sudo qm set 8000 --ide2 local-lvm:cloudinit
+sudo qm set 8000 --serial0 socket --vga serial0
+sudo qm set 8000 --agent enabled=1
+sudo qm template 8000
+rm jammy-server-cloudimg-amd64.img
+echo "Expandir disco"
+echo "copiar claves ssh"
